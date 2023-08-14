47e9c7d99c1629f5cdffc1aa093202793317ae76
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-08-14 15:19:35 +0300

    Create new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,4 +19,20 @@ router.get('/:id', (req, res) => {
     }
 })
 
+// Create new user
+router.post('/', (req,res) => {
+    const newUser = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+    }
+
+    if(!newUser.name || !newUser.email){
+        return res.sendStatus(400)
+    }
+
+    users.push(newUser)
+    res.json(users)
+})
+
 module.exports = router;
