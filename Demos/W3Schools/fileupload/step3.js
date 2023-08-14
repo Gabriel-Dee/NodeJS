6fb149ba1211e4179bf45da60ae507994244f031
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-08-14 17:10:13 +0300

    changes

--- Demos/W3Schools/fileupload/step3.js
+++ Demos/W3Schools/fileupload/step3.js
@@ -1,19 +1,20 @@
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+var path = require('path'); // Add this line to include the 'path' module
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+      var oldpath = files.filetoupload.path; // Use 'path' instead of 'filepath'
+      var newpath = path.join(__dirname, 'uploads', files.filetoupload.name); // Use 'path.join' for cross-platform path building
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
         res.end();
       });
- });
+    });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
