4082f35c9f636bbf8b5228d07b3917d1d532a162
Gabriel-Dee <gabriel.CSCU.2018@gmail.com>
2023-08-17 18:21:59 +0300

    Movies json file

---
+++ node/movies.js
@@ -0,0 +1,33 @@
+const movies =[
+    {
+      "id": 1,
+      "title": "The Shawshank Redemption",
+      "director": "Frank Darabont",
+      "release_date": "1994-09-10"
+    },
+    {
+      "id": 2,
+      "title": "The Godfather",
+      "director": "Francis Ford Coppola",
+      "release_date": "1972-03-24"
+    },
+    {
+      "id": 3,
+      "title": "The Dark Knight",
+      "director": "Christopher Nolan",
+      "release_date": "2008-07-18"
+    },
+    {
+      "id": 4,
+      "title": "Pulp Fiction",
+      "director": "Quentin Tarantino",
+      "release_date": "1994-10-14"
+    },
+    {
+      "id": 5,
+      "title": "The Lord of the Rings: The Return of the King",
+      "director": "Peter Jackson",
+      "release_date": "2003-12-17"
+    }
+  ]
+  
